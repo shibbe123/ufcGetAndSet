b7eaa78620af9a18b9c3f6d718ab38c3b9c5e21b
shibbe123 <122462709+shibbe123@users.noreply.github.com>
2023-02-23 14:46:03 +0100

    a

---
+++ ufcFighters/src/Fighters.java
@@ -0,0 +1,33 @@
+public class Fighters {
+    private String name;
+    private String origin;
+    private int age;
+    private int weight;
+    private double height;
+    private int reach;
+
+    public Fighters(String name, String origin, int age, int weight, double height, int reach){
+
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getOrigin(){
+        return origin;
+    }
+    public void setOrigin(String origin){
+        this.origin = origin;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    
+
+    
+}
